7b10c077f17776667a6acd9b2fdadd9565b022bd
jasrk11 <jas.rk11@gmail.com>
2016-05-23 23:25:13 +0530

    commit

---
+++ 15.071/Week5/tweets/tweets.R
@@ -0,0 +1,101 @@
+setwd("/home/musigma/MOOC/15.071/Week5/")
+tweets = read.csv("tweets.csv", stringsAsFactors = F)
+str(tweets)
+
+# For detecting only strongly negative tweets
+tweets$Negative <- as.factor(tweets$Avg <= -1) 
+
+library(tm)
+library(SnowballC)
+
+# Create corpus
+corpus = Corpus(VectorSource(tweets$Tweet))
+corpus
+
+
+corpus = tm_map(corpus, tolower)      # All chars to lowercase
+corpus = tm_map(corpus, PlainTextDocument)
+corpus = tm_map(corpus, removePunctuation)      # remove punctuation
+
+stopwords("english")[1:10]      # first 10 stopwords
+
+# Removing all stopwords & the word "apple" since all tweets have the word apple
+corpus = tm_map(corpus, removeWords, c("apple", stopwords("english")))
+
+# Stemming
+corpus = tm_map(corpus, stemDocument)
+
+# document term matrix where each row is a tweet and each column is frequency of the word
+frequencies <- DocumentTermMatrix(corpus)
+frequencies
+inspect(frequencies[1000:1005, 505:515])
+
+# this is sparse data, i.e., we have lot of zeroes in our data
+
+findFreqTerms(frequencies, lowfreq = 20)
+
+# remove less frequent terms
+# Reason1: Computationally expensive
+# Reason2: More independent variables = Less generalized model
+
+sparse = removeSparseTerms(frequencies, 0.995)
+sparse
+
+tweetsSparse = as.data.frame(as.matrix(sparse))
+
+# R struggles with variables that starts with number
+# Change word names to appropriate names
+colnames(tweetsSparse) = make.names(colnames(tweetsSparse))
+
+# Add dependent variable
+tweetsSparse$Negative <- tweets$Negative
+
+# Split data into train & test
+library(caTools)
+set.seed(123)
+split = sample.split(tweetsSparse$Negative, SplitRatio = 0.7)
+trainSparse = subset(tweetsSparse, split == TRUE)
+testSparse = subset(tweetsSparse, split == FALSE)
+
+
+# Build model using CART & Logisitic Regression
+library(rpart)
+library(rpart.plot)
+tweetCART = rpart(Negative~., data = trainSparse, method = "class")
+prp(tweetCART)
+
+# predict on test data
+predictCART = predict(tweetCART, newdata = testSparse, type = "class")
+table(testSparse$Negative, predictCART)
+#       predictCART
+#       FALSE TRUE
+# FALSE   294    6
+# TRUE     37   18
+
+# Accuracy
+(294+18)/(294+6+37+18)
+0.8788732
+
+# Baseline model
+table(testSparse$Negative)
+# FALSE  TRUE 
+# 300    55 
+(300)/(300+55)
+# 0.8450704
+
+# Random forest
+library(randomForest)
+set.seed(123)
+tweetRF = randomForest(Negative~., data = trainSparse)
+predictRF = predict(tweetRF, newdata = testSparse)
+table(testSparse$Negative, predictRF)
+#         FALSE TRUE
+# FALSE   293    7
+# TRUE     34   21
+(293+21)/(293+21+7+34)
+# 0.884507
+
+
+# Building Logisitic regression model
+library(glm)
+tweetLR = glm(Negative~., data = trainSparse, method = "binomial")
---
+++ 15.071/Week5/wiki/wiki.R
@@ -0,0 +1,104 @@
+setwd("/home/musigma/MOOC/15.071/Week5/wiki/")
+rm(list=ls())
+library(tm)
+library(SnowballC)
+
+
+wiki <- read.csv('wiki.csv', stringsAsFactors = F)
+wiki$Vandal <- as.factor(wiki$Vandal)
+
+# cases of vandalism
+table(wiki$Vandal)
+
+# corpus for added words
+corpusAdded = Corpus(VectorSource(wiki$Added))
+# Stopwords
+corpusAdded = tm_map(corpusAdded, removeWords, c(stopwords("english")))
+# stemming the words
+corpusAdded = tm_map(corpusAdded, stemDocument)
+
+# DocumentTermMatrix
+dtmAdded = DocumentTermMatrix(corpusAdded)
+# Terms-6675
+findFreqTerms(dtmAdded, lowfreq = 20)
+inspect(dtmAdded[2000:2010, 4000:4010])
+
+# Filter out sparse terms by keeping only terms that appear 
+# in 0.3% or more of the revision
+sparseAdded = removeSparseTerms(dtmAdded, 0.997)
+wordsAdded = as.data.frame(as.matrix(sparseAdded))
+colnames(wordsAdded) = paste("A", colnames(wordsAdded))
+
+# Repeat all the steps for removed words
+corpusRemoved = Corpus(VectorSource(wiki$Removed))
+# Stopwords
+corpusRemoved = tm_map(corpusRemoved, removeWords, c(stopwords("english")))
+# Stem words
+corpusRemoved = tm_map(corpusRemoved, stemDocument)
+
+# DocumenttemrMatrix
+dtmRemoved = DocumentTermMatrix(corpusRemoved)
+
+# Filter out sparse terms by keeping only terms that appear 
+# in 0.3% or more of the revision
+sparseRemoved = removeSparseTerms(dtmRemoved, sparse = 0.997)
+wordsRemoved = as.data.frame(as.matrix(sparseRemoved))
+colnames(wordsRemoved) = paste("R", colnames(wordsRemoved))
+
+# Combine the two data frames into a data frame 
+wikiWords = cbind(wordsAdded, wordsRemoved)
+wikiWords$Vandal <- wiki$Vandal
+
+library(caTools)
+set.seed(123)
+split = sample.split(wikiWords$Vandal, SplitRatio = 0.7)
+trainWiki = subset(wikiWords, split = TRUE)
+testWiki = subset(wikiWords, split = FALSE)
+table(testWiki$Vandal)
+2061/(1815+2061)
+# 0.5317337
+
+# Bulid a CART model
+library(rpart)
+library(rpart.plot)
+wikiCART <- rpart(Vandal~., data = trainWiki, method = "class")
+prp(wikiCART)
+
+#Accuracy
+predictCART = predict(wikiCART, newdata = testWiki, type = "class")
+table(testWiki$Vandal, predictCART)
+#      0    1
+# 0 2061    0
+# 1 1770   45
+(2061+45)/(2061+45+1+1770)
+0.5432035
+
+# baseline
+table(testWiki$Vandal)
+2061/(2061+1815)
+0.5317337
+
+# Problem-specific Knowledge
+# two techniques - identifying a key class of words, and counting words
+wikiWords2 = wikiWords
+wikiWords2$HTTP = ifelse(grepl("http", wiki$Added, fixed = TRUE), 1, 0)
+table(wikiWords2$HTTP)
+
+wikiTrain2 = subset(wikiWords2, split==TRUE)
+wikiTest2 = subset(wikiWords2, split==FALSE)
+# new CART
+wiki2CART <- rpart(Vandal~., data = wikiTrain2, method = "class")
+prp(wiki2CART)
+
+#Accuracy
+predict2CART = predict(wiki2CART, newdata = wikiTest2, type = "class")
+table(wikiTest2$Vandal, predict2CART)
+#     0   1
+# 0 609   9
+# 1 488  57
+#Accuracy
+(609+57)/(609+57+9+488)
+
+# Word count
+wikiWords2$NumWordsAdded = rowSums(as.matrix(dtmAdded))
+wikiWords2$NumWordsRemoved = rowSums(as.matrix(dtmRemoved))
